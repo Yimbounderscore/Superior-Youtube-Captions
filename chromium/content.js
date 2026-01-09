482350807dba0f827d1a98902f4ef373279598a0
Ali Diksin <mrblueover9000@gmail.com>
2026-01-09 15:59:05 +0000

    bug fixes and chromium port

--- chromium/content.js
+++ chromium/content.js
@@ -1,14 +1,14 @@
+// just inject the script that does the real work
 (function () {
     'use strict';
 
+    // log so we know we're alive
     console.log('[BetterYTSubs] CONTENT SCRIPT LOADED');
 
     // create a script tag pointing to our interceptor file
     function injectScript() {
         const script = document.createElement('script');
-        // use whatever browser api is available (chrome/firefox)
-        const runtimeAPI = typeof browser !== 'undefined' ? browser : chrome;
-        script.src = runtimeAPI.runtime.getURL('injected.js');
+        script.src = chrome.runtime.getURL('injected.js');
 
         // clean up after ourselves once it loads
         script.onload = function () {
